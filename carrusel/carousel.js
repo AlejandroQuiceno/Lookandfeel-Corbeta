0d00b486480beb2416bdef30582982d3ec6c494b
Alejandro Quiceno Mejia <alejandro.quiceno.m@gmail.com>
2023-06-30 10:49:38 -0500

    carousel test done :D

---
+++ carrusel/carousel.js
@@ -0,0 +1,27 @@
+const carouselContainer = document.querySelector('.carousel-container');
+const carouselItems = carouselContainer.querySelector('.carousel-items');
+const prevButton = carouselContainer.querySelector('.prev-button');
+const nextButton = carouselContainer.querySelector('.next-button');
+
+const itemWidth = carouselItems.firstElementChild.offsetWidth;
+const containerWidth = carouselContainer.offsetWidth;
+const itemCount = carouselItems.childElementCount;
+
+let currentIndex = 0;
+
+function updateCarousel() {
+  carouselItems.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % itemCount;
+  updateCarousel();
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + itemCount) % itemCount;
+  updateCarousel();
+}
+
+nextButton.addEventListener('click', nextSlide);
+prevButton.addEventListener('click', prevSlide);
